250a3f75cc53372d19b1e5b3f63773de92e0a6b3
Thiago Lemos <thiagolemos95@outlook.com>
2019-06-09 22:12:01 -0300

    Api

---
+++ Api2/Models/IProdutoRepositorio.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Api2.Models
+{
+    public interface IProdutoRepositorio
+    {
+        IEnumerable<Produto> GetAll();
+        Produto Get(int id);
+        Produto Add(Produto item);
+        void Remove(int id);
+        bool Update(Produto item);
+    }
+}
